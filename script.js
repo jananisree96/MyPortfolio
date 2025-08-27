46e1056ec501259ee9f37a8a1d056536a5cef38c
Janani Sree <153414043+jananisree96@users.noreply.github.com>
2025-08-27 17:17:31 +0530

    final commit updated code

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                document.querySelector(`header nav a[href*="${id}"]`).classList.add('active');
-            });
-        }
-    });
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector(`header nav a[href*="${id}"]`)
+          .classList.add("active");
+      });
+    }
+  });
 };
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-document.querySelectorAll(".accordion-header").forEach(button => {
+document.querySelectorAll(".accordion-header").forEach((button) => {
   button.addEventListener("click", () => {
     const content = button.nextElementSibling;
 
@@ -37,4 +39,3 @@ document.querySelectorAll(".accordion-header").forEach(button => {
     }
   });
 });
-
